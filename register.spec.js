c661f0d7130be8f207af8f8ed54df95d9e9eedac
MarioPuco <pucho76@gmail.com>
2024-05-02 16:36:19 +0200

    Register new user test

---
+++ register.spec.js
@@ -0,0 +1,23 @@
+//  Test function that declares tests and expect function to write assertions.
+import { test, expect } from '@playwright/test';
+
+test('Register new user', async ({ page }) => {
+  // Username and Passwored declared for easier reusability during testing
+  const username = "test321@tesingz.com";
+  const password = "RandomPassword321"
+    
+  await page.goto('https://magento.softwaretestingboard.com/');
+  await page.getByLabel('Consent', { exact: true }).click();
+  await page.getByRole('link', { name: 'Create an Account' }).click();
+  await page.getByLabel('First Name').click();
+  await page.getByLabel('First Name').fill(username);
+  await page.getByLabel('First Name').press('Tab');
+  await page.getByLabel('Last Name').fill('TestUser02');
+  await page.getByLabel('Email', { exact: true }).click();
+  await page.getByLabel('Email', { exact: true }).fill('test321@testing.com');
+  await page.getByRole('textbox', { name: 'Password*', exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password*', exact: true }).fill(password);
+  await page.getByRole('textbox', { name: 'Password*', exact: true }).press('Tab');
+  await page.getByLabel('Confirm Password').fill(password);
+  await page.getByRole('button', { name: 'Create an Account' }).click();
+});
