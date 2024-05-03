e8740faa8a53b3d6acbd3f8079105876ee1a3e66
MarioPuco <pucho76@gmail.com>
2024-05-03 12:13:47 +0200

    Changed username const, so email is not taken during the test

--- register.spec.js
+++ register.spec.js
@@ -3,15 +3,16 @@ import { test, expect } from '@playwright/test';
 
 test('Register new user', async ({ page }) => {
   // Username and Passwored declared for easier reusability during testing
-  const username = "test321@tesingz.com";
+  const username = "test3221@tesingz.com";
   const password = "RandomPassword321";
     
   await page.goto('https://magento.softwaretestingboard.com/');
   await page.getByLabel('Consent', { exact: true }).click();
   await page.getByRole('link', { name: 'Create an Account' }).click();
   await page.getByLabel('First Name').click();
-  await page.getByLabel('First Name').fill(username);
+  await page.getByLabel('First Name').fill("Random name");
   await page.getByLabel('First Name').press('Tab');
+  await page.getByLabel('Last Name').fill('TestUser02');
   await page.getByLabel('Email', { exact: true }).click();
   await page.getByLabel('Email', { exact: true }).fill(username);
   await page.getByRole('textbox', { name: 'Password*', exact: true }).click();
